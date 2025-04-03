8702bfb4628b4f22a58315fe9aff7d9bb27d31b6
lesia_onyshchuk <lkane443@gmail.com>
2025-04-03 12:02:53 +0300

    task 6

---
+++ src/scripts/clearProducts.js
@@ -0,0 +1,8 @@
+import fs from 'node:fs/promises';
+import { PATH_DB } from '../constants/products.js';
+
+const clearProducts = () => {
+  fs.writeFile(PATH_DB, JSON.stringify([]), 'utf-8');
+};
+
+clearProducts();
