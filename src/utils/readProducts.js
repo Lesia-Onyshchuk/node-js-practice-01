286d5a9d7008a27751d2d28fd8930da38f7ed766
lesia_onyshchuk <lkane443@gmail.com>
2025-04-08 20:36:47 +0300

    fix parseddata in readProducts

--- src/utils/readProducts.js
+++ src/utils/readProducts.js
@@ -3,5 +3,6 @@ import { PATH_DB } from '../constants/products.js';
 
 export const readProducts = async () => {
   const data = await fs.readFile(PATH_DB, 'utf-8');
-  return JSON.parse(data);
+  const parsedData = data ? JSON.parse(data) : [];
+  return parsedData;
 };
